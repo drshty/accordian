a730c5b767d1927d73c69d7a731581de2c41de47
drshty <drashtipatel0913@gmail.com>
2023-04-08 10:11:52 -0400

    created custom JS and CSS files

---
+++ main.js
@@ -0,0 +1,9 @@
+    "use strict";
+    $(document).ready(() => {
+        $("#accordion").accordion({
+            event: "click",
+            heightStyle: "content",
+            collapsible: true
+        })
+
+    });
